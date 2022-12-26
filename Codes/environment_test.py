cb21cde57caf1e1de6f75cffd634da16641f9727
Runze Yuan <1959180242@qq.com>
2022-12-26 17:44:03 +0000

    环境搭建与简单操作测试

---
+++ Codes/environment_test.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+# MinneApple path get:
+prev_path = os.path.abspath(os.path.join(os.getcwd(), ".."))
+MinneApplePath = prev_path + "\MinneApple"
+# MinneApple 文件夹里有三个文件夹： counting，detection，test_data
+counting_path = MinneApplePath + "\counting"
+detection_path = MinneApplePath + "\detection"
+test_data_path = MinneApplePath + (r"\test_data")
+
+
+#img = cv2.imread(test_data_path+(r"\counting\images\testset1_5cluster_12.jpg"))
+img = cv2.imread(r"A:\OneDrive\MScRobotics\MV\MV\MinneApple\test_data\test_data\counting\images\testset1_5cluster_12.jpg")
+plt.imshow(img)
