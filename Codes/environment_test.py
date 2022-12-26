3ce49fec87f6aa95e22b21e05ad24c8b3095c6ae
Runze Yuan <1959180242@qq.com>
2022-12-26 17:56:58 +0000

    图片路径生成

--- Codes/environment_test.py
+++ Codes/environment_test.py
@@ -1,18 +1,27 @@
+'''
+Author: Runze Yuan 1959180242@qq.com
+Date: 2022-12-26 17:23:51
+LastEditors: Runze Yuan 1959180242@qq.com
+LastEditTime: 2022-12-26 17:53:57
+FilePath: \MV\Codes\environment_test.py
+Description: 
+
+Copyright (c) 2022 by Runze Yuan 1959180242@qq.com, All Rights Reserved. 
+'''
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 
 # MinneApple path get:
-prev_path = os.path.abspath(os.path.join(os.getcwd(), ".."))
+prev_path = os.getcwd()
 MinneApplePath = prev_path + "\MinneApple"
 # MinneApple 文件夹里有三个文件夹： counting，detection，test_data
 counting_path = MinneApplePath + "\counting"
 detection_path = MinneApplePath + "\detection"
 test_data_path = MinneApplePath + (r"\test_data")
 
-
-#img = cv2.imread(test_data_path+(r"\counting\images\testset1_5cluster_12.jpg"))
-img = cv2.imread(r"A:\OneDrive\MScRobotics\MV\MV\MinneApple\test_data\test_data\counting\images\testset1_5cluster_12.jpg")
+print(prev_path)
+img = cv2.imread(test_data_path+(r"\counting\images\testset1_5cluster_12.jpg"))
 cv2.imshow("test",img)
 cv2.waitKey(0)
